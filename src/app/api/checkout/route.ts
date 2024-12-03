4bf97874ea249ac618fac560703990e1a554f34e
Yaman Aliesh <alyaman@thresholdsa.net>
2024-12-04 00:37:37 +0300

    feat:Add endpiont to checkout

---
+++ src/app/api/checkout/route.ts
@@ -0,0 +1,60 @@
+import { db } from "@/lib/db";
+import { stripe } from "@/lib/stripe";
+import { NextRequest, NextResponse } from "next/server";
+import Stripe from "stripe";
+
+const corsHeaders = {
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Methods": "GET,POST,PUT,DELETE,OPTIONS",
+  "Access-Control-Allow-Headers": "X-Requested-With,Content-Type,Authorization",
+};
+
+export async function OPTIONS() {
+  return NextResponse.json({}, { headers: corsHeaders });
+}
+
+export async function POST(
+  req: NextRequest,
+  { params }: { params: { storeId: string } }
+) {
+  try {
+    const { productIds } = await req.json();
+    if (!productIds || productIds.length === 0) {
+      return NextResponse.json("Product ids are required", { status: 400 });
+    }
+
+    const products = await db.product.findMany({
+      where: { id: { in: productIds } },
+    });
+
+    const line_items: Stripe.Checkout.SessionCreateParams.LineItem[] = [];
+
+    products.forEach((product) => {
+      line_items.push({
+        quantity: 1,
+        price_data: {
+          currency: "USD",
+          product_data: {
+            name: product.name,
+          },
+          unit_amount: product.price * 100,
+        },
+      });
+    });
+
+    const session = await stripe.checkout.sessions.create({
+      line_items,
+      mode: "payment",
+      billing_address_collection: "required",
+      phone_number_collection: {
+        enabled: true,
+      },
+      success_url: `http://localhost:3000/`,
+      cancel_url: `http://localhost:3000/`,
+    });
+
+    return NextResponse.json({ url: session.url }, { headers: corsHeaders });
+  } catch (error) {
+    console.log("error", error);
+  }
+}
