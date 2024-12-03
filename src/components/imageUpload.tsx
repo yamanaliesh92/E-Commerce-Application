9c9764f6e2fca6d7b6c2b0eace45093f16b6f946
Yaman Aliesh <alyaman@thresholdsa.net>
2024-12-03 23:49:38 +0300

    feat:Add imageUplaod  component

---
+++ src/components/imageUpload.tsx
@@ -0,0 +1,80 @@
+"use client";
+import { ImagePlus, Trash } from "lucide-react";
+import Image from "next/image";
+import React, { useEffect, useState } from "react";
+import { CldUploadWidget } from "next-cloudinary";
+import { useIntl } from "react-intl";
+
+interface UploadImageProps {
+  value: string;
+  disabled?: boolean;
+  onRemove: () => void;
+  onChange: (value: string) => void;
+}
+
+export default function ImageUpload({
+  value,
+  onChange,
+  onRemove,
+  disabled,
+}: UploadImageProps) {
+  const { formatMessage } = useIntl();
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => setIsMounted(true), []);
+
+  const onUpload = (result: any) => {
+    console.log("onUpload done", result);
+    onChange(result.info.secure_url);
+  };
+
+  if (!isMounted) {
+    return null;
+  }
+
+  return (
+    <div className="flex items-center gap-2">
+      <CldUploadWidget uploadPreset="j0am2dyf" onSuccess={onUpload}>
+        {({ open }) => {
+          const onClick = () => {
+            open();
+          };
+          return (
+            <button
+              disabled={disabled}
+              type="button"
+              onClick={onClick}
+              className="bg-gray-700 dark:bg-white dark:text-black text-white ps-4 pe-4 py-2 rounded-lg transition duration-300 flex items-center gap-2"
+            >
+              <ImagePlus className="w-4 h-4" />
+              {formatMessage({ id: "imageUpload.uploadImage" })}
+            </button>
+          );
+        }}
+      </CldUploadWidget>
+
+      <div className="flex  items-center gap-4">
+        {value && (
+          <div className="relative rounded-md overflow-hidden">
+            <div className="z-10 absolute top-[0px] end-[-5px]">
+              <button
+                type={"button"}
+                onClick={onRemove}
+                className="bg-red-500 p-1 rounded-full"
+              >
+                <Trash className="h-4 w-4" />
+              </button>
+            </div>
+            <Image
+              alt="Image"
+              width={200}
+              height={200}
+              className="h-[50px] w-[50px] rounded-full object-cover"
+              src={value}
+            />
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
---
+++ src/components/modal/productForm.tsx
@@ -0,0 +1,146 @@
+"use client";
+import React, { useState } from "react";
+import { useForm, Controller } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+import axios from "axios";
+import { useIntl } from "react-intl";
+import CustomInput from "./input";
+import ImageUpload from "./ImageUpload";
+import { FaTimes } from "react-icons/fa";
+import Button from "./button";
+import Link from "next/link";
+import { toast } from "react-hot-toast";
+import { useRouter } from "next/navigation";
+import useFetchUser from "@/hooks/useFetchUser";
+
+interface IProductForm {
+  type: "update" | "create";
+  data?: any;
+  setOpen?: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+export const ProductForm = ({ type, data, setOpen }: IProductForm) => {
+  const { formatMessage } = useIntl();
+  const router = useRouter();
+  const [isLoading, setIsLoading] = useState(false);
+  const user = useFetchUser();
+
+  const productSchema = z.object({
+    name: z.string().min(1, formatMessage({ id: "productForm.nameRequired" })),
+    description: z
+      .string()
+      .min(1, formatMessage({ id: "productForm.descriptionRequired" })),
+    price: z
+      .number()
+      .positive(formatMessage({ id: "productForm.pricePositive" })),
+    imageUrl: z.string().url(formatMessage({ id: "productForm.invalidUrl" })),
+  });
+
+  const title =
+    type === "create"
+      ? formatMessage({ id: "productForm.create" })
+      : formatMessage({ id: "productForm.update" });
+
+  type ProductFormInputs = z.infer<typeof productSchema>;
+
+  const {
+    register,
+    handleSubmit,
+    control,
+    formState: { errors },
+  } = useForm<ProductFormInputs>({
+    resolver: zodResolver(productSchema),
+    defaultValues: data,
+  });
+
+  const onSubmit = async (formData: ProductFormInputs) => {
+    try {
+      if (type === "create") {
+        setIsLoading(true);
+        await axios.post("/api/product", { ...formData, userId: user?.id });
+        setIsLoading(false);
+        toast.success(formatMessage({ id: "productForm.productCreated" }));
+        router.refresh();
+        router.push("/");
+      } else {
+        setIsLoading(true);
+        await axios.patch(`/api/product/${data.id}`, formData);
+        toast.success(formatMessage({ id: "productForm.productUpdated" }));
+        setIsLoading(false);
+        router.refresh();
+        setOpen && setOpen(false);
+      }
+    } catch (err) {
+      console.error("Error creating product", err);
+      toast.error(formatMessage({ id: "productForm.unknownError" }));
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  return (
+    <div className="flex items-center justify-center z-[100] h-[100vh] fixed inset-0 w-full bg-black bg-opacity-50">
+      <div className="p-6 w-[500px] bg-white dark:bg-gray-700 rounded-lg shadow-md">
+        <div className="flex justify-between mb-3">
+          <h2 className="text-2xl dark:text-white font-bold">{title}</h2>
+          {type === "create" ? (
+            <Link href={"/"}>
+              <FaTimes className="dark:text-white" size={20} />
+            </Link>
+          ) : (
+            <FaTimes
+              onClick={() => setOpen && setOpen(false)}
+              className="dark:text-white"
+              size={20}
+            />
+          )}
+        </div>
+
+        <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
+          <div>
+            <CustomInput
+              {...register("name")}
+              label={formatMessage({ id: "productForm.name" })}
+              error={errors.name?.message}
+            />
+          </div>
+          <div>
+            <CustomInput
+              {...register("description")}
+              label={formatMessage({ id: "productForm.description" })}
+              error={errors.name?.message}
+              isDescription={true}
+            />
+          </div>
+          <div>
+            <CustomInput
+              {...register("price", { valueAsNumber: true })}
+              label={formatMessage({ id: "productForm.price" })}
+              error={errors.price?.message}
+            />
+          </div>
+          <Controller
+            name="imageUrl"
+            control={control}
+            render={({ field }) => (
+              <ImageUpload
+                value={field.value as any}
+                onChange={field.onChange}
+                onRemove={() => field.onChange("")}
+                disabled={false}
+              />
+            )}
+          />
+          {errors.imageUrl && (
+            <p className="text-red-500">{errors.imageUrl.message}</p>
+          )}
+
+          <Button isLoading={isLoading} type="submit">
+            {title}
+          </Button>
+        </form>
+      </div>
+    </div>
+  );
+};
