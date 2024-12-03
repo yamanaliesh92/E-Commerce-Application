fa4fe9714853efb443f04973f533a70674397178
Yaman Aliesh <alyaman@thresholdsa.net>
2024-12-04 00:41:46 +0300

    feat:Update  next.config file

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      { protocol: "https", hostname: "res.cloudinary.com" },
+      { protocol: "https", hostname: "images.pexels.com" },
+    ],
+  },
 };
 
 export default nextConfig;
