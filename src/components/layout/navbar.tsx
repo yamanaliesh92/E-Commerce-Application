2267f8491ff4c7c72a37bb22819c0a9355fe177c
Yaman Aliesh <alyaman@thresholdsa.net>
2024-12-03 23:37:56 +0300

    feat:Add userMenu  component

---
+++ src/components/layout/navbar.tsx
@@ -0,0 +1,92 @@
+"use client";
+import { useState, useEffect } from "react";
+import CartPage from "./cart";
+
+import ThemeSwitch from "./ThemeSwitch";
+import { useIntl } from "react-intl";
+import LanguageSwitcher from "./switch";
+import useFetchUser from "@/hooks/useFetchUser";
+import axios from "axios";
+import UserMenu from "./UserMenu";
+import BurgerMenu from "./burgerMenu";
+
+const Navbar: React.FC = () => {
+  const [open, setOpen] = useState(false);
+  const [scrolling, setScrolling] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
+  const { formatMessage } = useIntl();
+  const user = useFetchUser();
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setScrolling(window.scrollY > 0);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  const onSubmit = async () => {
+    try {
+      await axios.post("/api/user/logout");
+      alert(formatMessage({ id: "navbar.logout" }));
+    } catch (err) {
+      console.error("Error logging out", err);
+      alert("An error occurred while logging out");
+    }
+  };
+
+  const closeMenu = () => {
+    setOpen(false);
+  };
+
+  return (
+    <>
+      <header
+        className={`p-4 h-16 fixed top-0 w-full bg-white dark:bg-black ${
+          scrolling ? "border-b dark:border-b-black shadow-md" : ""
+        } transition-all duration-300 z-50`}
+      >
+        <div className="container mx-auto flex justify-between items-center">
+          <div className="flex items-center">
+            <span className="font-bold text-2xl sm:text-3xl uppercase text-blue-500">
+              xk<span className="text-yellow-400">shop</span>
+            </span>
+          </div>
+          <div className="flex items-center gap-1 sm:gap-4">
+            <LanguageSwitcher />
+            <ThemeSwitch />
+            <div className="hidden sm:flex items-center gap-2 sm:gap-6">
+              <UserMenu
+                closeMenu={closeMenu}
+                user={user}
+                setOpen={setOpen}
+                onSubmit={onSubmit}
+              />
+            </div>
+            <BurgerMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+          </div>
+        </div>
+      </header>
+
+      {menuOpen && (
+        <div className="sm:hidden absolute top-16 left-4 z-10 w-full bg-white dark:bg-gray-700 p-4">
+          <div className="flex flex-col gap-4">
+            <UserMenu
+              closeMenu={closeMenu}
+              user={user}
+              setOpen={setOpen}
+              onSubmit={onSubmit}
+            />
+          </div>
+        </div>
+      )}
+
+      {open && <CartPage setOpen={setOpen} />}
+    </>
+  );
+};
+
+export default Navbar;
---
+++ src/components/layout/userMenu.tsx
@@ -0,0 +1,98 @@
+"use client";
+import Link from "next/link";
+import { FaShoppingCart, FaEdit, FaPlus } from "react-icons/fa";
+import { useIntl } from "react-intl";
+
+interface UserMenuProps {
+  user: {
+    id: number;
+    email: string;
+    username: string;
+    createdAt: Date;
+  } | null;
+  setOpen: React.Dispatch<React.SetStateAction<boolean>>;
+  onSubmit: () => void;
+  closeMenu: () => void;
+}
+
+const UserMenu: React.FC<UserMenuProps> = ({
+  user,
+  closeMenu,
+  setOpen,
+  onSubmit,
+}) => {
+  const { formatMessage } = useIntl();
+
+  const handleOpen = () => {
+    setOpen((prev) => !prev);
+  };
+
+  return (
+    <>
+      {!user ? (
+        <>
+          <Link
+            className="bg-transparent rounded-md border text-blue-500 border-yellow-400 ps-4 pe-4 py-2 transition duration-300 hover:bg-yellow-400 hover:text-black"
+            href="/sign-up"
+            onClick={closeMenu}
+          >
+            {formatMessage({ id: "navbar.signUp" })}
+          </Link>
+          <Link
+            className="rounded-md text-white bg-yellow-400 ps-4 pe-4 py-2 transition duration-300 hover:bg-yellow-500 hover:text-black"
+            href="/login"
+            onClick={closeMenu}
+          >
+            {formatMessage({ id: "navbar.login" })}
+          </Link>
+        </>
+      ) : (
+        <>
+          <div className="group relative flex items-center">
+            <Link
+              href={`/dashboard/${user.id}`}
+              className="text-blue-500 transition duration-300 dark:text-white"
+              onClick={closeMenu}
+            >
+              <FaEdit size={24} />
+              <span className="hidden group-hover:block absolute dark:text-black dark:bg-white bg-black text-white text-xs rounded py-1 px-2 bottom-full mb-1">
+                {formatMessage({ id: "navbar.dashboard" })}
+              </span>
+            </Link>
+          </div>
+          <div className="group relative flex items-center">
+            <button
+              onClick={handleOpen}
+              className="text-blue-500 transition duration-300 dark:text-white"
+            >
+              <FaShoppingCart size={24} />
+              <span className="hidden group-hover:block absolute dark:text-black dark:bg-white bg-black text-white text-xs rounded py-1 px-2 bottom-full mb-1">
+                {formatMessage({ id: "navbar.addToCart" })}
+              </span>
+            </button>
+          </div>
+          <div className="group relative flex items-center">
+            <Link
+              href="/product/create"
+              className="text-blue-500 transition duration-300 dark:text-white"
+              onClick={closeMenu}
+            >
+              <FaPlus size={24} />
+              <span className="hidden group-hover:block absolute dark:bg-white dark:text-black bg-black text-white text-xs rounded py-1 px-2 bottom-full mb-1">
+                {formatMessage({ id: "navbar.createProduct" })}
+              </span>
+            </Link>
+          </div>
+          <button
+            className="bg-transparent rounded-md border text-blue-500 border-yellow-400 ps-4 pe-4 py-2 transition duration-300 hover:bg-yellow-400 hover:text-black"
+            onClick={onSubmit}
+          >
+            {formatMessage({ id: "navbar.logout" })}
+          </button>
+        </>
+      )}
+    </>
+  );
+};
+
+export default UserMenu;
