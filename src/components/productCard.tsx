6a5412cc5c7b37fed0cee0eee790ff72449f6350
Yaman Aliesh <alyaman@thresholdsa.net>
2024-12-03 23:53:49 +0300

    feat:Add Search  component

---
+++ src/components/productCard.tsx
@@ -0,0 +1,115 @@
+"use client";
+import { addToCart } from "@/redux/cart";
+import React, { useEffect, useState } from "react";
+import { FaTrash, FaEdit, FaCartPlus } from "react-icons/fa";
+import { useDispatch } from "react-redux";
+import Image from "next/image";
+import { useIntl } from "react-intl";
+
+import DeleteModal from "./deleteModal";
+import { ProductForm } from "./create";
+import useFetchUser from "@/hooks/useFetchUser";
+import toast from "react-hot-toast";
+
+interface ProductCardProps {
+  product: {
+    id: number;
+    name: string;
+    description: string;
+    price: number;
+    imageUrl: string;
+    userId: number;
+  };
+}
+
+const ProductCard: React.FC<ProductCardProps> = ({ product }) => {
+  const [isMounted, setIsMounted] = useState(false);
+
+  useEffect(() => setIsMounted(true), []);
+
+  const dispatch = useDispatch();
+  const { formatMessage } = useIntl();
+
+  const [openEditModel, setOpenEditModel] = useState(false);
+  const [openDeleteModal, setOpenDeleteModal] = useState(false);
+  const user = useFetchUser();
+
+  const addCart = () => {
+    dispatch(
+      addToCart({
+        description: product.description,
+        imageUrl: product.imageUrl,
+        id: product.id,
+        price: product.price,
+        name: product.name,
+        quantity: 1,
+      })
+    );
+    toast.success(formatMessage({ id: "cart.itemAdded" }));
+  };
+
+  const truncateDescription = (desc: string, maxLength: number) => {
+    if (desc.length <= maxLength) {
+      return desc;
+    }
+    return `${desc.substring(0, maxLength)}...`;
+  };
+
+  return (
+    <div className="bg-white dark:bg-gray-700 shadow-lg rounded-lg p-4 relative">
+      <Image
+        src={product.imageUrl}
+        alt={product.name}
+        width={200}
+        height={200}
+        className="w-full h-64 object-cover rounded-t-lg"
+      />
+      <div className="p-4">
+        <h3 className="text-xl dark:text-white font-bold">{product.name}</h3>
+        <p className="text-gray-700 dark:text-yellow-50 h-[45px] mt-2">
+          {truncateDescription(product.description, 100)}
+        </p>
+        <p className="text-green-500 text-lg font-semibold mt-2">
+          ${product.price}
+        </p>
+        <div className="flex justify-between mt-4">
+          <div className="flex items-center gap-2">
+            {user?.id === product.userId && (
+              <>
+                <button
+                  onClick={() => setOpenDeleteModal(true)}
+                  className="bg-red-500 p-2 rounded-full text-white"
+                >
+                  <FaTrash className="w-4 h-4" />
+                </button>
+                <button
+                  onClick={() => setOpenEditModel((prev) => !prev)}
+                  className="bg-gray-500 dark:bg-white p-2 rounded-full dark:text-black text-white"
+                >
+                  <FaEdit className="w-4 h-4" />
+                </button>
+              </>
+            )}
+          </div>
+          <button
+            onClick={addCart}
+            className="text-blue-500 dark:bg-white  p-2 rounded-full"
+          >
+            <FaCartPlus className="w-6 h-6" />
+            <span className="sr-only">
+              {formatMessage({ id: "productCard.addToCart" })}
+            </span>
+          </button>
+        </div>
+      </div>
+      {openEditModel && (
+        <ProductForm type="update" setOpen={setOpenEditModel} data={product} />
+      )}
+      {openDeleteModal && (
+        <DeleteModal id={product.id} setOpen={setOpenDeleteModal} />
+      )}
+    </div>
+  );
+};
+
+export default ProductCard;
---
+++ src/components/search.tsx
@@ -0,0 +1,41 @@
+"use client";
+
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import React, { useEffect, useState } from "react";
+import { FaSearch } from "react-icons/fa";
+import { useIntl } from "react-intl";
+import { useDebounce } from "./useDebunce";
+
+export default function SearchInput() {
+  const { formatMessage } = useIntl();
+  const [value, setValue] = useState("");
+  const debouncedValue = useDebounce(value);
+
+  const searchParams = useSearchParams();
+  const router = useRouter();
+  const pathName = usePathname();
+
+  useEffect(() => {
+    const queryParams = new URLSearchParams(searchParams.toString());
+    if (debouncedValue) {
+      queryParams.set("title", debouncedValue);
+    } else {
+      queryParams.delete("title");
+    }
+
+    const url = `${pathName}?${queryParams.toString()}`;
+    router.push(url);
+  }, [debouncedValue, searchParams, router, pathName]);
+
+  return (
+    <div className="relative mt-2 gap-[10px]">
+      <FaSearch className="h-4 w-4 absolute top-3 start-3 text-black dark:text-white" />
+      <input
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+        placeholder={formatMessage({ id: "search.placeholder" })}
+        className="w-full h-[45px] md:w-[300px] dark:border-white placeholder:dark:text-white placeholder:text-black ps-9 rounded-full bg-slate-300 dark:bg-gray-700 focus-visible:ring-slate-200"
+      />
+    </div>
+  );
+}
